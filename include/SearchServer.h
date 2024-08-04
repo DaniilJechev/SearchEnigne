9c2bafed297bb8d1dec238e552db1fb2a1d2c5d1
DaniilJechev <daniilzhechev.bfg@gmail.com>
2024-08-04 16:15:16 +0300

    Create and tested SearchServer

--- include/SearchServer.h
+++ include/SearchServer.h
@@ -5,17 +5,24 @@
 
 #include "InvertedIndex.h"
 
-struct RelativeIndex{
-    size_t doc_id;
-    float rank;
-    bool operator ==(const RelativeIndex& other) const;
+struct RelativeIndex {
+    size_t m_docId;
+    float m_rank;
+
+    bool operator==(const RelativeIndex &other) const;
+
+    RelativeIndex(size_t docId, float rank) :
+            m_docId(docId), m_rank(rank) {};
 };
 
 class SearchServer {
 public:
-    explicit SearchServer(InvertedIndex& idx) : m_index(idx){};
+    explicit SearchServer(InvertedIndex& freqDict) : m_freqDict(freqDict){};
     std::vector<std::vector<RelativeIndex>> search(const
                 std::vector<std::string>& queries_input);
+    static void addIndex (std::vector<RelativeIndex>& docRelativeIdx, const Entry& newIdx);
+    static void convertAbsToRelative(std::vector<RelativeIndex>& docRelativeIdx); //array must be sorted
+
 private:
-    InvertedIndex& m_index;
+    InvertedIndex& m_freqDict;
 };
--- src/SearchServer.cpp
+++ src/SearchServer.cpp
@@ -1,11 +1,55 @@
 #include "SearchServer.h"
+#include "InvertedIndex.h"
+
 #include <vector>
 #include <string>
+#include <sstream>
+#include <utility>
+#include <algorithm>
 
 bool RelativeIndex::operator==(const RelativeIndex &other) const {
-    return (doc_id == other.doc_id && rank == other.rank);
+    return (m_docId == other.m_docId && m_rank == other.m_rank);
 }
 
-std::vector<std::vector<RelativeIndex>> SearchServer::search(const std::vector<std::string> &queriesInput) {
-
+std::vector<std::vector<RelativeIndex>>SearchServer::search(const std::vector<std::string> &queriesInput) {
+    std::vector<std::vector<RelativeIndex>> result;
+    for (const auto &query: queriesInput) {
+        std::string word;
+        std::stringstream stream(query);
+        std::vector<RelativeIndex> docRelativeIdx;
+        while (stream >> word) {
+            for (const auto &it: m_freqDict.GetWordCount(word)) {
+                SearchServer::addIndex(docRelativeIdx, it);
+            }
+        }
+        std::sort(docRelativeIdx.begin(), docRelativeIdx.end(),
+                  [](const RelativeIndex &first, const RelativeIndex &second) {
+                        if (first.m_rank == second.m_rank) {
+                            return (first.m_docId < second.m_docId);
+                        }
+                        return (first.m_rank > second.m_rank);
+                  });
+        convertAbsToRelative(docRelativeIdx);
+        result.push_back(std::move(docRelativeIdx));
+    }
+    return result;
 }
+
+void SearchServer::addIndex(std::vector<RelativeIndex> &docRelativeIdx, const Entry &newIdx) {
+    for (auto &it: docRelativeIdx) {
+        if (it.m_docId == newIdx.m_docId) {
+            it.m_rank += static_cast<float>(newIdx.m_count);
+            return;
+        }
+    }
+    docRelativeIdx.emplace_back(newIdx.m_docId,static_cast<float>(newIdx.m_count));
+}
+
+void SearchServer::convertAbsToRelative(std::vector<RelativeIndex> &docRelativeIdx){ //array must be sorted
+    if (docRelativeIdx.empty()) return;
+    float maxIdx = docRelativeIdx[0].m_rank;
+    for (auto &it : docRelativeIdx) {
+        it.m_rank /= maxIdx;
+    }
+}
+
